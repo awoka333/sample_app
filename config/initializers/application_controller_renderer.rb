b642d408c77e017abcde0163ccb677bd93cc12d2
awoka333 <na23okzw.htre@gmail.com>
2020-10-25 11:26:31 +0000

    added all functions

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '36f52b73b7cfe7e38a1c346b2e165fab5649cfdcc65b65be0f61801c793ac9cd56fb6358c6f09060438986893824690130d548dda84ef43228d5c50299b7c3a6'
